e6b71afeaf4f0c707a1d1193e7282f9f069134f3
Prabhjot Singh <prabhjot.singh57@t-mobile.com>
2018-07-18 09:31:19 -0700

    demo of ng-content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   serverElements = [
     { type: 'server', name: 'testserver', content: 'just a test' },
     { type: 'blueprint', name: 'server', content: 'just a test' }
@@ -38,5 +39,22 @@ export class AppComponent {
     }
   }
 
+  constructor() { }
+
+  // ngOnChanges() {}  // called after inbound changes.
+
+  ngOnInit() {}  // called once component is intialized.
+
+  // ngDoCheck(){} // called every change detected.
+
+  // ngAfterContentInit() {} // called after content (ng-content) has been projected in view
+
+  // ngAfterContentChecked() {} // called every time the projected content has been checked
+
+  // ngAfterViewInit() {} // called after the componenet view (and child views) has been initialized
+
+  // ngAfterViewChecked() {} called every time (and child views) have been checked
+
+  // ngOnDestroy() {} // called when coponenet is about to be destroyed.
 
 }
